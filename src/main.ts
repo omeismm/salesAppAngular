59f80663c7c9757f7c99a2f857a010532ac50646
omeismm <128798266+omeismm@users.noreply.github.com>
2024-08-17 14:05:53 +0300

    renamed the app from app to sales

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 //to start, type "ng serve" in the terminal
+
 //if environment is production, enable production mode
-import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment'; //i need to write this file manually, see 2:46:55 in the video  
-if (environment.production) {
-  enableProdMode();
-}
+//import { enableProdMode } from '@angular/core';
+//import { environment } from './environments/environment'; //i need to write this file manually, see 2:46:55 in the video  
+//if (environment.production) {
+ // enableProdMode();
+//}
 
 
 bootstrapApplication(AppComponent, appConfig)//i need to create app module 2:57:00
